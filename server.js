18b276c0b2c9e00bc6a181e5e42a9db27dfdab20
Justus Sturkenboom <justus@ju5tu5.nl>
2024-05-14 10:36:07 +0200

    POST werkend gemaakt

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // console.log('Hier komt je server voor Sprint 10. Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
 
-
 /*** Express setup & start ***/
 
 // Importeer het npm pakket express uit de node_modules map
 import express, { application, json } from 'express'
+
 import cookieParser from 'cookie-parser'
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
 // Maak een nieuwe express app aan
 const app = express()
+const apiUrl = 'https://fdnd-agency.directus.app/items/'
+const apiFamily = apiUrl + 'oba_family'
+const apiProfile = apiUrl + 'oba_profile'
+const apiItem = apiUrl + 'oba_item'
+
+let leeslijst = {}
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
@@ -24,14 +30,6 @@ app.use(cookieParser())
 
 app.use(express.urlencoded({ extended: true }))
 
-const apiUrl = "https://fdnd-agency.directus.app/items/"
-
-const apiFamily = (apiUrl + 'oba_family')
-
-const apiProfile = (apiUrl + 'oba_profile')
-
-const apiItem = (apiUrl + 'oba_item')
-
 // dit is de index pagina // met alle items van OBA
 // hier vraag ik om de afbeeldingen in te laden met de originele width en height
 
@@ -39,21 +37,16 @@ app.get('/', function (request, response) {
   fetchJson(apiItem + '?fields=*,afbeelding.id,afbeelding.height,afbeelding.width').then((items) => {
     console.log(items.data)
     response.render('index', {
-
-      items: items.data/*hier zeg ik dat iedereen getoond moet worden*/
-    });
+      items: items.data /*hier zeg ik dat iedereen getoond moet worden*/,
+    })
   })
 })
 
-
 // books
-
-
 app.get('/home', async function (request, response) {
-
   try {
-    const families = await fetchJson(apiFamily);
-    const profiles = await fetchJson(apiProfile);
+    const families = await fetchJson(apiFamily)
+    const profiles = await fetchJson(apiProfile)
 
     //   console.log(families.data);
     //   console.log(profiles.data);
@@ -61,170 +54,153 @@ app.get('/home', async function (request, response) {
     response.render('home', {
       families: families.data,
       profiles: profiles.data,
-    });
+    })
   } catch (error) {
     //   console.error('Error fetching data:', error);
-    response.status(500).send('Internal Server Error');
+    response.status(500).send('Internal Server Error')
   }
-});
+})
 
 // detail
-
 app.get('/detail/:id', function (request, response) {
   fetchJson(apiItem + '?filter={"id":' + request.params.id + '}').then((items) => {
     response.render('detail', {
-
-      items: items.data/*hier zeg ik dat iedereen getoond moet worden*/
-    });
+      items: items.data /*hier zeg ik dat iedereen getoond moet worden*/,
+    })
   })
-
 })
 
-// leeslijst
+app.post('/detail/:id', function (request, response) {
+  const id = request.params.id
 
+  console.log(request.params.id)
 
+  // posten naar directus..
+  fetch(`${apiUrl}oba_bookmarks/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item: request.params.id,
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  }).then((postResponse) => {
+    console.log(postResponse)
+    response.redirect(303, '/detail/' + id + '?added=true')
+  })
+})
 
-let leeslijst = {}
+app.get('/leeslijst', function (request, response) {
+  let loadingStatus = 'loading' // Indicate that the page is loading
 
-
-app.post ('/detail/:id', function(request, response) {
-   const id = request.params.id;
-
-
-   leeslijst[id] = true;
-
-
-   response.redirect(303, '/detail/' + id + '?added=true');
-      
-});
-
-
-app.get('/leeslijst', function(request, response) {
-  let loadingStatus = "loading"; // Indicate that the page is loading
+  // bookmarks ophalen...
+  let leeslijstFetch = `${apiUrl}oba_bookmarks?fields=*.*`
 
   // Simulate an asynchronous task, such as an API call
-  fetchJson(apiItem)
-  .then((items) => {
-      console.log(items.data);
-
-      let itemsOpLeeslijst = [];
-
-      items.data.forEach(function(item) {
-          if (leeslijst[item.id]) {
-              itemsOpLeeslijst.push(item);
-          }
-      });
-
-      // If there are items, render the list
+  fetchJson(leeslijstFetch)
+    .then(({ data }) => {
+      return data.map((bookmark) => {
+        return bookmark.item
+      })
+    })
+    .then((itemsOpLeeslijst) => {
       if (itemsOpLeeslijst.length) {
-        console.log("there is a book")
-        console.log(loadingStatus);
-          response.render('leeslijst', {
-              items: itemsOpLeeslijst,
-              loadingStatus: loadingStatus
-          });
+        // console.log('there is a book')
+        // console.log(loadingStatus)
+        response.render('leeslijst', {
+          items: itemsOpLeeslijst,
+          loadingStatus: loadingStatus,
+        })
       } else {
-          console.log("empty state")
-          // If there are no items, render the empty state
-          response.render('partials/emptyState');
+        // console.log('empty state')
+        // If there are no items, render the empty state
+        response.render('partials/emptyState')
       }
-  })
-  .catch((error) => {
-      console.error("Error fetching items:", error);
-      response.status(500).send("Internal Server Error");
-  });
-});
-
+    })
+})
 
 // app.get('/leeslijst', function (request, response) {
 //   fetchJson(apiItem).then((items) => {
 //     let loadingStatus = "loading";
 //     let itemsOpLeeslijst = []
-    // console.log(request.cookies.leeslijst)
-    // console.log(request.cookies);
-    // items.data.forEach(function (item) {
-      // turn id into string so it corresponds with the cookie ids;
-    //   const id = `${item.id}`
-    //   if (request.cookies.leeslijst && request.cookies.leeslijst.includes(id)) {
-    //     itemsOpLeeslijst.push(item)
-    //     console.log('gets here')
-    //   }
-    // })
+// console.log(request.cookies.leeslijst)
+// console.log(request.cookies);
+// items.data.forEach(function (item) {
+// turn id into string so it corresponds with the cookie ids;
+//   const id = `${item.id}`
+//   if (request.cookies.leeslijst && request.cookies.leeslijst.includes(id)) {
+//     itemsOpLeeslijst.push(item)
+//     console.log('gets here')
+//   }
+// })
 
-    // if (itemsOpLeeslijst.length) {
-    //   response.render('leeslijst', {
-    //     items: itemsOpLeeslijst
-    //   });
+// if (itemsOpLeeslijst.length) {
+//   response.render('leeslijst', {
+//     items: itemsOpLeeslijst
+//   });
 
+// if (itemsOpLeeslijst.length === 0) {
+//   loadingStatus = "empty";
+// } else {
+//   loadingStatus = "success";
+// }
 
-      // if (itemsOpLeeslijst.length === 0) {
-      //   loadingStatus = "empty";
-      // } else {
-      //   loadingStatus = "success";
-      // }
+//   response.render('leeslijst', {
+//     loadingStatus: loadingStatus,
+//     items: itemsOpLeeslijst
+//   });
 
-    //   response.render('leeslijst', {
-    //     loadingStatus: loadingStatus,
-    //     items: itemsOpLeeslijst
-    //   });
+//   if (itemsOpLeeslijst.length === 0) {
+//     // Rendeer de lege staat
+//     response.render('emptyState');
+// } else {
+//     // Rendeer de items
+//     response.render('leeslijst', {
+//         items: itemsOpLeeslijst
+//     });
+// }
 
-
-      //   if (itemsOpLeeslijst.length === 0) {
-      //     // Rendeer de lege staat
-      //     response.render('emptyState');
-      // } else {
-      //     // Rendeer de items
-      //     response.render('leeslijst', {
-      //         items: itemsOpLeeslijst
-      //     });
-      // }
-
-    // } else {
-    //   response.render('leeslijst', {
-    //     loadingStatus: loadingStatus,
-    //     items: []
-    //   });
-      // console.error("Invalid or unexpected API response format");
-      // response.status(500).send("Internal Server Error");
+// } else {
+//   response.render('leeslijst', {
+//     loadingStatus: loadingStatus,
+//     items: []
+//   });
+// console.error("Invalid or unexpected API response format");
+// response.status(500).send("Internal Server Error");
 //     }
 //   });
 // });
-
 
 // let leeslijst = []
 
 // app.post('/detail/:id', function (request, response) {
 //   request.cookies.leeslijst = JSON.parse(request.cookies.leeslijst)
-  
+
 //   if (request.cookies.leeslijst) {
 //     leeslijst = request.cookies.leeslijst
 //     console.log(leeslijst)
-//   } 
+//   }
 //   fetchJson(apiItem + '?filter={"id":' + request.params.id + '}').then((items) => {
 //     const book =  items.data[0]
-    // console.log(book)
-    // leeslijst.push(request.params.id)
-    // Jammerdepammer:
-    // leeslijst[id] = {
-    //   id: book.id,
-    //   title: book.title,
-    //   afbeelding: book.afbeelding,
-    // };
-    // Save leeslijst in cookie called 'leeslijst'
-  //   console.log('lijst:' + leeslijst);
-  //   response.cookie('leeslijst', leeslijst, { maxAge: 900000, httpOnly: false });
-  //   response.redirect(303, '/detail/' + request.params.id + '?added=true');
-  // })
+// console.log(book)
+// leeslijst.push(request.params.id)
+// Jammerdepammer:
+// leeslijst[id] = {
+//   id: book.id,
+//   title: book.title,
+//   afbeelding: book.afbeelding,
+// };
+// Save leeslijst in cookie called 'leeslijst'
+//   console.log('lijst:' + leeslijst);
+//   response.cookie('leeslijst', leeslijst, { maxAge: 900000, httpOnly: false });
+//   response.redirect(303, '/detail/' + request.params.id + '?added=true');
+// })
 
-
-  // send succes state // 
+// send succes state //
 
 // });
 
-
 // favorieten
-
-
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
@@ -233,4 +209,4 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
-});
+})
